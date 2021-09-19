d127c6a6b2bee9f7ebea8d66023654432ef5f142
choi yejun <yejun4911@naver.com>
2021-09-19 15:56:58 +0900

    sort

---
+++ Java/CodingTest/src/BJ_11650.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BJ_11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0]=Integer.parseInt(st.nextToken());
+            arr[i][1]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for(int[] s : arr){
+            System.out.print(s[0]+" ");
+            System.out.println(s[1]);
+        }
+    }
+}
---
+++ Java/CodingTest/src/BJ_11651.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0]=Integer.parseInt(st.nextToken());
+            arr[i][1]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            } else {
+                return e1[1] - e2[1];
+            }
+        });
+
+        for(int[] s : arr){
+            System.out.print(s[0]+" ");
+            System.out.println(s[1]);
+        }
+    }
+}
