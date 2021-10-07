9d3e1c75ab23e2094ae59751b84faffe616c1c20
choi yejun <yejun4911@naver.com>
2021-10-07 18:31:18 +0900

    stack

---
+++ Java/CodingTest/src/BJ_10773.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ_10773 {
+    static int[] arr;
+    static int pointer=0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            int temp = Integer.parseInt(br.readLine());
+            if(temp==0){
+                pop();
+            }else{
+                push(temp);
+            }
+        }
+        System.out.println(Arrays.stream(arr).sum());
+    }
+    private static void push(int num){
+        arr[pointer++]=num;
+
+    }
+    private static void pop(){
+
+        arr[pointer-1]=0;
+        pointer--;
+
+    }
+}
