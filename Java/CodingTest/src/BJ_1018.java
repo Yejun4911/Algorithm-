d9221c98495676a771c76e3204c2ec97a939cbb5
choi yejun <yejun4911@naver.com>
2021-10-20 14:47:40 +0900

    brute force

--- Java/CodingTest/src/BJ_1018.java
+++ Java/CodingTest/src/BJ_1018.java
@@ -2,12 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.io.IOException;
-
+/* 21-10-20  */
 public class BJ_1018 {
-
-    public static boolean[][] arr;
-    public static int min = 64;
-
+    static char[][] arr;
+    static int min = 64;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,73 +15,40 @@ public class BJ_1018 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        arr = new boolean[N][M];
+        arr = new char[N][M];
 
-
-        // 배열 입력
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-
-            for (int j = 0; j < M; j++) {
-                if (str.charAt(j) == 'W') {
-                    arr[i][j] = true;		// W일 때는 true
-                } else {
-                    arr[i][j] = false;		// B일 때는 false
-                }
-
+        for(int i=0; i<N; i++){ // 입력 값을 배열에 저장
+            String temp = br.readLine();
+            for(int j=0; j<M; j++){
+                arr[i][j]=temp.charAt(j);
             }
         }
 
+        int N_row = N - 7; //  브루트 포스하기 위해 8*8의 모든 영역을 조회
+        int M_col = M - 7; //
 
-        int N_row = N - 7;
-        int M_col = M - 7;
-
-        for (int i = 0; i < N_row; i++) {
+        for (int i = 0; i < N_row; i++) { // 모든 영역을 조회하기 위해, 시작점 설정
             for (int j = 0; j < M_col; j++) {
-                find(i, j);
+                chess(i, j);
             }
         }
         System.out.println(min);
     }
-
-
-    public static void find(int x, int y) {
-        int end_x = x + 8;
-        int end_y = y + 8;
+    private static void chess(int x,int y){
+        char fristColor = arr[x][y]; // 첫번째 값 저장
         int count = 0;
+        for(int i=x; i<x+8; i++){ // 시작지점 부터 8*8 조회
+            for(int j=y; j<y+8; j++){
+                if(fristColor!=arr[i][j]) count++; // 첫번째 값과 다른 경우 count++
 
-        boolean TF = arr[x][y];	// 첫 번째 칸의 색
-
-        for (int i = x; i < end_x; i++) {
-            for (int j = y; j < end_y; j++) {
-
-                // 올바른 색이 아닐경우 count 1 증가
-                if (arr[i][j] != TF) {
-                    count++;
-                }
-
-                /*
-                 * 다음 칸은 색이 바뀌므로
-                 * true라면 false로, false 라면 true 로
-                 * 값을 변경한다.
-                 */
-                TF = (!TF);
+                if(fristColor=='B') fristColor='W'; // fristColor 색을 바꿔줌, 다음 값과 비교하기 위해
+                else fristColor='B';
             }
-
-            TF = !TF;
+            if(fristColor=='B') fristColor='W';
+            else fristColor='B';
         }
+        count = Math.min(count,64-count); // white, black 둘 중 어느 색으로 바꿔야 할지 최소 값 검색
 
-        /*
-         *  첫 번째 칸을 기준으로 할 때의 색칠 할 개수(count)와
-         *  첫 번째 칸의 색의 반대되는 색을 기준으로 할 때의
-         *  색칠 할 개수(64 - count) 중 최솟값을 count 에 저장
-         */
-        count = Math.min(count, 64 - count);
-
-        /*
-         * 이전까지의 경우 중 최솟값보다 현재 count 값이
-         * 더 작을 경우 최솟값을 갱신
-         */
-        min = Math.min(min, count);
+        min = Math.min(min,count);
     }
 }
