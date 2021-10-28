8f3d1767e9a90c015460512327b91c9fc926e398
choi yejun <yejun4911@naver.com>
2021-10-28 20:19:50 +0900

    level1

---
+++ Java/CodingTest/src/programmers/MathSqrt.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class MathSqrt {
+    public static void main(String[] args) {
+        solution(121);
+    }
+    public static long solution(long n) {
+        long answer = 0;
+        if ((int)Math.sqrt(n) == Math.sqrt(n)) {
+            int temp = (int)Math.sqrt(n);
+            temp++;
+            answer = temp * temp;
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
