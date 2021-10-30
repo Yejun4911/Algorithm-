29dc1a7c8fa36b3cebef91c75d09f99a2a7c8327
choi yejun <yejun4911@naver.com>
2021-10-30 17:08:05 +0900

    level1

---
+++ Java/CodingTest/src/programmers/StringChangeInt.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class StringChangeInt {
+    public static void main(String[] args) {
+        solution("1234");
+    }
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
