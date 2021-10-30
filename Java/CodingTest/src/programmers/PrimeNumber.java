48d082332c4f000d73cd35c793dada0dee30cee4
choi yejun <yejun4911@naver.com>
2021-10-30 18:13:35 +0900

    level1

--- Java/CodingTest/src/programmers/PrimeNumber.java
+++ Java/CodingTest/src/programmers/PrimeNumber.java
@@ -6,17 +6,20 @@ public class PrimeNumber {
     }
     public static int solution(int n) {
         int answer = 0;
-        for (int i = 2; i <= n; i++) {
-            int count = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
-            }
-            if (count == 2) {
-                answer++;
+        boolean[] prime = new boolean[n+1];
+        prime[0] = prime[1] = true;
+
+        for(int i = 2 ; i <= Math.sqrt(prime.length); i++){
+            if(prime[i]) continue;
+            for(int j = i*i ; j < prime.length; j+=i){
+                prime[j] = true;
             }
         }
+
+        for (int i = 0; i < prime.length; i++) {
+            if (!prime[i]) answer++;
+        }
+
         return answer;
     }
 }
