b726a8ae4176a9d7b72ac2383da7c574d651ec5a
choi yejun <yejun4911@naver.com>
2021-11-02 16:58:41 +0900

    level1

---
+++ Java/CodingTest/src/programmers/TwoNumberSum.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class TwoNumberSum {
+    public static void main(String[] args) {
+        solution(3,5);
+    }
+    public static long solution(int a, int b) {
+        long answer = 0;
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        for (int i = a; i <= b; i++) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
