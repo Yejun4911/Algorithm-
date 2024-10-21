0f8cacab117e0d577775457043ad5f09e1b63334
yejun4911 <yejun4911@naver.com>
2024-10-21 13:38:21 +0900

    Algorithm BackJoon

--- Java/CodingTest/src/newAlgorithm/BJ1654.java
+++ Java/CodingTest/src/newAlgorithm/BJ1654.java
@@ -3,7 +3,7 @@ package newAlgorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ1654 {
     public static void main(String[] args) throws IOException {
