a7b3f56b0d8f808360a0d9dfd602687701612a2e
yejun4911 <yejun4911@naver.com>
2024-10-22 11:29:25 +0900

    Algorithm BackJoon

---
+++ Java/CodingTest/src/newAlgorithm/BJ1009.java
@@ -0,0 +1,29 @@
+package newAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BJ1009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb =  new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int result = 1;
+            for (int j = 0; j < b; j++) {
+                result = (result*a)%10;
+            }
+            if(result==0) System.out.println("10");
+            else System.out.println(result);
+        }
+
+    }
+}
