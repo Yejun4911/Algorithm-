add3de3fc1577ca20fc68e3a9ebf537b7a5acb66
yejun4911 <yejun4911@naver.com>
2024-10-28 17:57:26 +0900

    Algorithm BackJoon

---
+++ Java/CodingTest/src/newAlgorithm/BJ1032.java
@@ -0,0 +1,31 @@
+package newAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1032 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] list = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            list[i] = br.readLine();
+        }
+
+        char[] temp = list[0].toCharArray();
+        for (int i = 1; i <N; i++ ){
+
+            for (int j =0; j<temp.length; j++) {
+                if (temp[j] != list[i].charAt(j)) {
+                    temp[j] = '?';
+                }
+            }
+        }
+        System.out.println(temp);
+    }
+}
